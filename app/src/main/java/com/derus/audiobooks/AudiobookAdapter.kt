fe8cc46dc3a103e00f1d6d1c5a2e62e667bcfc84
MichalDerus <michalderus@poczta.onet.pl>
2017-09-08 19:27:38 +0200

    added searching

--- app/src/main/java/com/derus/audiobooks/AudiobookAdapter.kt
+++ app/src/main/java/com/derus/audiobooks/AudiobookAdapter.kt
@@ -4,21 +4,32 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
 import com.futuremind.recyclerviewfastscroll.SectionTitleProvider
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.item_list.view.*
-import java.util.*
 
-/**
- * Created by Michal on 31.08.2017.
- */
-class AudiobookAdapter(val resultsList: ArrayList<AudiobookResponse>, val listener: OnBookClickListener) : RecyclerView.Adapter<AudiobookAdapter.ViewHolder>(), SectionTitleProvider {
-    override fun getSectionTitle(position: Int): String {
-        return resultsList.get(position).title.substring(0,1)
+
+class AudiobookAdapter(val resultsList: ArrayList<AudiobookResponse>, val listener: OnBookClickListener) : RecyclerView.Adapter<AudiobookAdapter.ViewHolder>(),
+        SectionTitleProvider, Filterable {
+
+    var filteredList: ArrayList<AudiobookResponse> = resultsList
+    var audiobookFilter: AudiobookFilter? = null
+
+
+    override fun getFilter(): Filter {
+        if (audiobookFilter == null){
+            audiobookFilter = AudiobookFilter()
+        }
+        return audiobookFilter!!
     }
 
+    override fun getSectionTitle(position: Int): String =
+            filteredList.get(position).title.substring(0,1)
+
     override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
-        holder?.bindAudiobooks(resultsList[position])
+        holder?.bindAudiobooks(filteredList[position])
     }
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): AudiobookAdapter.ViewHolder {
@@ -26,9 +37,8 @@ class AudiobookAdapter(val resultsList: ArrayList<AudiobookResponse>, val listen
         return ViewHolder(v, listener)
     }
 
-    override fun getItemCount(): Int {
-        return resultsList.size
-    }
+    override fun getItemCount(): Int =
+            filteredList.size
 
     class ViewHolder(itemView: View, val listener: OnBookClickListener) : RecyclerView.ViewHolder(itemView) {
         fun bindAudiobooks(book: AudiobookResponse) {
@@ -38,5 +48,33 @@ class AudiobookAdapter(val resultsList: ArrayList<AudiobookResponse>, val listen
                 Picasso.with(itemView.context).load(path).into(itemView.image_item_book)
                 super.itemView.setOnClickListener { listener.OnBookClick(book.href, path, book.title, book.author)}
         }
+    }
+
+    inner class AudiobookFilter: Filter() {
+        override fun performFiltering(constraint: CharSequence?): FilterResults {
+            val filterResult = FilterResults()
+
+            if (constraint != null && constraint.length > 0){
+                val tempList = ArrayList<AudiobookResponse>()
+                for (book: AudiobookResponse in resultsList){
+                    if (book.title.toLowerCase().contains(constraint.toString().toLowerCase()) ||
+                            book.author.toLowerCase().contains(constraint.toString().toLowerCase()))
+                        tempList.add(book)
+                }
+                filterResult.count = tempList.size
+                filterResult.values = tempList
+
+            }else{
+                filterResult.count = resultsList.size
+                filterResult.values = resultsList
+            }
+            return filterResult
+        }
+
+        override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
+            filteredList = results?.values as ArrayList<AudiobookResponse>
+            notifyDataSetChanged()
+        }
+
     }
 }
--- app/src/main/java/com/derus/audiobooks/MainActivity.kt
+++ app/src/main/java/com/derus/audiobooks/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.derus.audiobooks
 
+import android.app.SearchManager
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.SearchView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.alert
@@ -14,7 +16,19 @@ import retrofit2.Callback
 import retrofit2.Response
 import java.io.File
 
-class MainActivity : AppCompatActivity(), OnBookClickListener {
+
+
+
+class MainActivity : AppCompatActivity(), OnBookClickListener, SearchView.OnQueryTextListener {
+
+    lateinit var audiobookAdapter: AudiobookAdapter
+
+    override fun onQueryTextSubmit(query: String?): Boolean = true
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+        audiobookAdapter.filter.filter(newText)
+        return true
+    }
 
     override fun OnBookClick(url: String, imageUrl: String, title: String, author: String) {
         //Toast.makeText(applicationContext, url, Toast.LENGTH_SHORT).show()
@@ -38,7 +52,8 @@ class MainActivity : AppCompatActivity(), OnBookClickListener {
         recyclerView.layoutManager = LinearLayoutManager(this)
         val api = ApiService()
         val list = ArrayList<AudiobookResponse>()
-        recyclerView.adapter = AudiobookAdapter(list, this)
+        audiobookAdapter = AudiobookAdapter(list, this)
+        recyclerView.adapter = audiobookAdapter
         recyclerView.setHasFixedSize(true)
         fastscroll.setRecyclerView(recyclerView)
 
@@ -60,6 +75,19 @@ class MainActivity : AppCompatActivity(), OnBookClickListener {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         super.onCreateOptionsMenu(menu)
         menuInflater.inflate(R.menu.main, menu)
+
+/*        val searchManager = getSystemService(SEARCH_SERVICE) as SearchManager
+        val searchMenuItem = menu?.findItem(R.id.search)
+        val searchView = searchMenuItem?.getActionView()!! as SearchView*/
+
+        val searchManager = getSystemService(SEARCH_SERVICE) as SearchManager
+        val searchView = menu!!.findItem(R.id.search).actionView as? SearchView
+        searchView?.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+
+
+        searchView?.setSubmitButtonEnabled(true)
+        searchView?.setOnQueryTextListener(this)
+
         return true
     }
 
